81b4c2e5c9e12af9db217761d5ed6ba2cdc789a5
Matthäus Zloch <matthaeus.zloch@gmail.com>
2024-09-30 17:06:55 +0200

    AIP-269: extend tests to test workflow step 2

--- tests/workflows/test_workflows.py
+++ tests/workflows/test_workflows.py
@@ -1,13 +1,13 @@
 from unittest.mock import patch
 
 import pytest
-from sqlalchemy import create_engine, text
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import and_, create_engine, or_, text
+from sqlalchemy.orm import joinedload, sessionmaker
 from sqlalchemy.pool import StaticPool
 
 from workflows.db.helpers import get_datetime_now
-from workflows.db.tables import Base, User
-from workflows.workflow import mark_deletion_candidates
+from workflows.db.tables import Base, Conversation, Message, User
+from workflows.workflow import delete_candidate_user_data, mark_deletion_candidates
 
 # Create a session for a SQLite in-memory database
 engine = create_engine(
@@ -56,6 +56,9 @@ def test_user_count(get_db_session):
         ).count()  # Query the number of users in the User table
         assert user_count == 5, f"Expected 5 users, but found {user_count}"
 
+        deletion_candidates = db.query(User).filter(User.wf_deletion_candidate).count()
+        assert deletion_candidates == 2
+
 
 @pytest.fixture
 def mock_user_data(get_db_session):
@@ -64,6 +67,12 @@ def mock_user_data(get_db_session):
         users = session.query(User).all()
 
     return users  # Return the user objects for testing
+
+
+@pytest.fixture
+def mock_log():
+    with patch("workflows.workflow.log") as mock_log:
+        yield mock_log
 
 
 @patch("workflows.workflow.get_db_session")
@@ -100,6 +109,9 @@ def test_mark_deletion_candidates(
 
     # check the database for changes
     with get_db_session as session:
+        for user in session.query(User).all():
+            print(f"{user.email} -> {user.wf_deletion_candidate}")
+
         # query for the users who should be marked for deletion
         user = session.query(User).filter(User.email == "user2@example.com").first()
         assert (
@@ -110,7 +122,72 @@ def test_mark_deletion_candidates(
         ), "User 2 should have the correct deletion timestamp."
 
         # assert that other users are not marked for deletion
-        for user in session.query(User).filter(User.email != "user2@example.com"):
+        for user in session.query(User).filter(
+            and_(
+                User.email != "user2@example.com",
+                User.email != "user4@example.com",
+                User.email != "user5@example.com",
+            )
+        ):
             assert (
                 user.wf_deletion_candidate is False
             ), f"{user.email} should not be marked for deletion."
+
+
+@patch("workflows.workflow.get_db_session")
+def test_delete_candidate_user_data_success(
+    mock_get_db_session, mock_log, get_db_session
+):
+    """"""
+    # Mock the get_db_session to return the in-memory test session
+    mock_get_db_session.return_value = get_db_session
+
+    # call the function under test
+    delete_candidate_user_data()
+
+    # assert success logging
+    for user in [{"email": f"user{i}@example.com"} for i in [4, 5]]:
+        mock_log.info.assert_any_call(
+            f"Successfully deleted data for user {user['email']}."
+        )
+
+    # check the database for changes
+    with get_db_session as session:
+        # query for the users who should be marked for deletion
+        delete_candidate_users = (
+            session.query(User).filter(User.wf_deletion_candidate).all()
+        )
+        assert len(delete_candidate_users) == 0
+
+        # assertion: no user data should be associated with the deleted users
+        user_data = (
+            session.query(User)
+            .options(
+                joinedload(User.conversations).joinedload(
+                    Conversation.messages
+                ),  # Eager load messages in conversations
+                joinedload(User.folders),  # Eager load models related to the user
+            )
+            .filter(
+                or_(
+                    Conversation.userEmail == "user4@example.com",
+                    Conversation.userEmail == "user5@example.com",
+                )
+            )
+            .one_or_none()
+        )
+        assert user_data is None
+
+        # assertion: no messages should be associated with the deleted users
+        messages = (
+            session.query(Message)
+            .join(Conversation, Message.conversationId == Conversation.id)
+            .filter(
+                or_(
+                    Conversation.userEmail == "user4@example.com",
+                    Conversation.userEmail == "user5@example.com",
+                )
+            )
+            .all()
+        )
+        assert len(messages) == 0
