9ce1533af461d3cd9c338336631a263f54712344
Matthäus Zloch <matthaeus.zloch@gmail.com>
2024-10-04 14:21:51 +0200

    AIP-187 (fix): pre-commit

--- rtl_rag_chatbot_api/oauth/get_current_user.py
+++ rtl_rag_chatbot_api/oauth/get_current_user.py
@@ -1,12 +1,9 @@
 import os
 from typing import Optional
-from fastapi import FastAPI, Depends, HTTPException
-from jose import jwt, JWTError
-from pydantic import BaseModel
 
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
-import requests
+from jose import JWTError, jwt
 
 from rtl_rag_chatbot_api.oauth.get_public_key import get_public_key
 
@@ -22,9 +19,11 @@ async def get_current_user(token: str = Depends(oauth2_scheme)) -> Optional[str]
     )
     try:
         header = jwt.get_unverified_header(token)
-        public_key = get_public_key(header['kid'])
+        public_key = get_public_key(header["kid"])
         # AUDIENCE = CLIENT_ID in single tenant apps
-        payload = jwt.decode(token, public_key, algorithms=["RS256"], audience=os.getenv("CLIENT_ID"))
+        payload = jwt.decode(
+            token, public_key, algorithms=["RS256"], audience=os.getenv("CLIENT_ID")
+        )
         print(payload)
         username: str = payload.get("sub")
         if username is None:
